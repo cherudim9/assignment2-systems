a61cf28d80fbd5bfd15eb602b16920cc815eeba2
cherudim9 <cherudim9@gmail.com>
2025-08-29 00:40:56 -0700

    flash attention forward v1

--- cs336_systems/flash_attention.py
+++ cs336_systems/flash_attention.py
@@ -1,9 +1,11 @@
 import math
 import torch
+import triton
+import triton.language as tl
 from einops import rearrange
 
-Bq = 2
-Bk = 2
+Bq = 16
+Bk = 16
 
 class FlashAttentionFunc(torch.autograd.Function):
     @staticmethod
@@ -51,3 +53,134 @@ class FlashAttentionFunc(torch.autograd.Function):
     @staticmethod
     def backward(ctx, grad_out):
         raise NotImplementedError
+
+
+@triton.jit
+def flash_fwd_kernel(
+    Q_ptr, K_ptr, V_ptr,
+    O_ptr, L_ptr,
+    stride_qb, stride_qq, stride_qd,
+    stride_kb, stride_kk, stride_kd,
+    stride_vb, stride_vk, stride_vd,
+    stride_ob, stride_oq, stride_od,
+    stride_lb, stride_lq,
+    N_QUERIES, N_KEYS,
+    scale,
+    D: tl.constexpr,
+    Q_TILE_SIZE: tl.constexpr,
+    K_TILE_SIZE: tl.constexpr,
+):
+
+    query_tile_index = tl.program_id(0)
+    batch_index = tl.program_id(1)
+    
+    # Offset each pointer with the corresponding batch index\
+    # multiplied with the batch stride for each tensor
+    Q_block_ptr = tl.make_block_ptr(
+        Q_ptr + batch_index * stride_qb,
+        shape=(N_QUERIES, D),
+        strides=(stride_qq, stride_qd),
+        offsets=(query_tile_index * Q_TILE_SIZE, 0),
+        block_shape=(Q_TILE_SIZE, D),
+        order=(1, 0),
+    )
+    qi = tl.load(Q_block_ptr, boundary_check=(0,1), padding_option="zero")
+
+    K_block_ptr = tl.make_block_ptr(
+        K_ptr + batch_index * stride_kb,
+        shape=(N_KEYS, D),
+        strides=(stride_kk, stride_kd),
+        offsets=(0, 0),
+        block_shape=(K_TILE_SIZE, D),
+        order=(1, 0),
+    )
+
+    V_block_ptr = tl.make_block_ptr(
+        V_ptr + batch_index * stride_vb,
+        shape=(N_KEYS, D),
+        strides=(stride_vk, stride_vd),
+        offsets=(0, 0),
+        block_shape=(K_TILE_SIZE, D),
+        order=(1, 0),
+    )
+
+    o = tl.zeros((Q_TILE_SIZE, D), dtype=tl.float32)
+    l = tl.zeros((Q_TILE_SIZE,), dtype=tl.float32)
+    m = tl.full((Q_TILE_SIZE,), float('-inf'), dtype=tl.float32)
+
+    for _ in range(tl.cdiv(N_KEYS, K_TILE_SIZE)):
+        kj = tl.load(K_block_ptr, boundary_check=(0,1), padding_option="zero")
+        vj = tl.load(V_block_ptr, boundary_check=(0,1), padding_option="zero")
+
+        s = tl.dot(qi, kj) * scale
+        prev_m = m
+        m = tl.maximum(prev_m, tl.max(s, axis=1))
+        p = tl.exp(s - m.expand_dims(axis=1))
+        l = l * tl.exp(prev_m - m) + tl.sum(p, axis=1)
+        o = (prev_m - m).expand_dims(axis=1) * o
+        tl.dot(p, vj, acc=o)
+
+        K_block_ptr = K_block_ptr.advance((K_TILE_SIZE, 0))
+        V_block_ptr = V_block_ptr.advance((K_TILE_SIZE, 0))
+
+    O_block_ptr = tl.make_block_ptr(
+        O_ptr + batch_index * stride_ob,
+        shape=(N_QUERIES, D),
+        strides=(stride_oq, stride_od),
+        offsets=(query_tile_index * Q_TILE_SIZE, 0),
+        block_shape=(Q_TILE_SIZE, D),
+        order=(1, 0),
+    )
+
+    L_block_ptr = tl.make_block_ptr(
+        L_ptr + batch_index * stride_lb,
+        shape=(N_QUERIES,),
+        strides=(stride_lq,),
+        offsets=(query_tile_index * Q_TILE_SIZE,),
+        block_shape=(Q_TILE_SIZE,),
+        order=(0,),
+    )
+
+    tl.store(O_block_ptr, o, boundaray_check=(0,1))
+    tl.store(L_block_ptr, l, boundaray_check=(0,))
+
+
+class FlashAttentionTritonFunc(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, Q, K, V, is_causal=False):
+        Q_shape = Q.shape
+        Q = rearrange(Q, "... seq_len d -> (...) seq_len d")
+        K = rearrange(K, "... seq_len d -> (...) seq_len d")
+        V = rearrange(V, "... seq_len d -> (...) seq_len d")
+        
+        bs, N_QUERIES, D = Q.shape
+        scale = d ** -0.5
+        Tq = math.ceil(N_QUERIES / Bq)
+        bs, N_KEYS, d = K.shape
+
+        O = torch.empty_like(Q)
+        L = torch.empty((bs, nq,))
+
+        flash_fwd_kernel[(tl.cdiv(bs), Tq)](
+            Q, K, V,
+            O, L,
+            Q.stride(0), Q.stride(1), Q.stride(2),
+            K.stride(0), K.stride(1), K.stride(2),
+            V.stride(0), V.stride(1), V.stride(2),
+            O.stride(0), O.stride(1), O.stride(2),
+            L.stride(0), L.stride(1),
+            N_QUERIES=N_QUERIES, N_KEYS=N_KEYS,
+            scale=scale,
+            D=D,  
+            Q_TILE_SIZE=Bq,
+            K_TILE_SIZE=Bk,
+        )
+
+        L = L.view(Q_shape[:-1])
+        ctx.save_for_backward(L)
+
+        O = O.view(Q_shape)
+        return O
+
+
+
